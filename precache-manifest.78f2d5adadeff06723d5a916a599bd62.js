dd4726fc49fd4f149988f38b9d4c522cbb0dcb54
dataskeptic <mateus_henrique2011@outlook.com>
2024-10-17 15:28:13 -0300

    Updates

---
+++ precache-manifest.78f2d5adadeff06723d5a916a599bd62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4acbb27d308404633022c6ebcd0c02",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b430b73d34703a7ec13f",
+    "url": "/portfolio/static/css/2.7a519478.chunk.css"
+  },
+  {
+    "revision": "aac239f6dcf3003b212b",
+    "url": "/portfolio/static/css/main.719cc264.chunk.css"
+  },
+  {
+    "revision": "b430b73d34703a7ec13f",
+    "url": "/portfolio/static/js/2.ecde1511.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.ecde1511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac239f6dcf3003b212b",
+    "url": "/portfolio/static/js/main.048451d5.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  }
+]);
